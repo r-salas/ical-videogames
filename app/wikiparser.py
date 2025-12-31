b0f80a95b4a1d0e80d09a93fdf2d169d8279773b
Rubén Salas <ruben.sls@outlook.com>
2025-12-31 12:52:53 +0100

    Add delay to prevent blocking

--- app/wikiparser.py
+++ app/wikiparser.py
@@ -4,6 +4,8 @@
 #
 #
 
+import time
+
 import bs4
 import requests
 
@@ -119,5 +121,9 @@ def iterate_games(platforms: Iterable[Platform]):
                     game = wiki_row_to_game(row, platform)
                     platform_games.add(game)
 
+                time.sleep(30)   # Add delay to prevent blocking
+
         for game in platform_games:
             yield game
+
+        time.sleep(30)   # Add delay to prevent blocking
